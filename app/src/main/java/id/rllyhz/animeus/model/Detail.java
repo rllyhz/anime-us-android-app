4663f901a2a24b673817ea7ca109474efac6abb9
rllyhz <rullyihza00@gmail.com>
2021-01-01 16:41:05 +0700

    Adding Detail model file for Anime and Manga model file

---
+++ app/src/main/java/id/rllyhz/animeus/model/Detail.java
@@ -0,0 +1,47 @@
+package id.rllyhz.animeus.model;
+
+public class Detail {
+    private int id;
+    private String type, name, url;
+
+    public Detail() {}
+
+    public Detail(int id, String type, String name, String url) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
