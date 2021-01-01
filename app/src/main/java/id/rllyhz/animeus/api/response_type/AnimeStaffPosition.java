621550c3eae5175e8fd50f7b2464c58c0f509433
rllyhz <rullyihza00@gmail.com>
2021-01-01 22:05:31 +0700

    Adding response type of for detail information in order to retrieve a character by given id

--- app/src/main/java/id/rllyhz/animeus/api/response_type/AnimeStaffPosition.java
+++ app/src/main/java/id/rllyhz/animeus/api/response_type/AnimeStaffPosition.java
@@ -1,4 +1,27 @@
 package id.rllyhz.animeus.api.response_type;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class AnimeStaffPosition {
+    @SerializedName("position")
+    @Expose
+    private String position;
+
+    @SerializedName("anime")
+    @Expose
+    private VoiceActingRole.Anime anime;
+
+    public AnimeStaffPosition(String position, VoiceActingRole.Anime anime) {
+        this.position = position;
+        this.anime = anime;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public VoiceActingRole.Anime getAnime() {
+        return anime;
+    }
 }
