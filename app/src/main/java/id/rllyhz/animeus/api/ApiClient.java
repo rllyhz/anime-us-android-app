562e2f30bff8b8d387f3a805deab7c7d28b9ff93
rllyhz <rullyihza00@gmail.com>
2021-01-01 16:53:02 +0700

    Adding Response Type file for retrieving anime by id

--- app/src/main/java/id/rllyhz/animeus/api/ApiClient.java
+++ app/src/main/java/id/rllyhz/animeus/api/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     private static Retrofit retrofit;
-    public static final String BASEURL = "https://api.jikan.moe/v3/";
+    public static final String BASEURL = "https://api.jikan.moe/v3";
 
     public static Retrofit getApiClientInstance() {
         if (retrofit == null) {
--- app/src/main/java/id/rllyhz/animeus/api/data_service/AnimeAPIService.java
+++ app/src/main/java/id/rllyhz/animeus/api/data_service/AnimeAPIService.java
@@ -1,4 +1,10 @@
 package id.rllyhz.animeus.api.data_service;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AnimeAPIService {
+
+    @GET("/anime/{id}/")
+    Call
 }
