3e65d0aa2f1a879cfc0974cd00ba580bc56cd217
rllyhz <rullyihza00@gmail.com>
2021-01-01 20:59:09 +0700

    Deleting example photoAPIService implementation

--- app/src/main/java/id/rllyhz/animeus/api/ApiClient.java
+++ app/src/main/java/id/rllyhz/animeus/api/ApiClient.java
@@ -1,15 +1,13 @@
 package id.rllyhz.animeus.api;
 
 import id.rllyhz.animeus.api.data_service.AnimeAPIService;
-import id.rllyhz.animeus.api.data_service.PhotoAPIService;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     private static Retrofit animeApiServiceInstance;
 
-    public static Retrofit getAnimeApiServiceInstance()
-    {
+    public static Retrofit getAnimeApiServiceInstance() {
         if (animeApiServiceInstance == null) {
             animeApiServiceInstance = new Retrofit.Builder()
                     .baseUrl(AnimeAPIService.BASE_URL)
