b23576057011cb0843002680ca38e76a563f2744
rllyhz <rullyihza00@gmail.com>
2021-01-01 22:59:22 +0700

    Adding MainActivity

--- app/src/main/java/id/rllyhz/animeus/SplashScreen.java
+++ app/src/main/java/id/rllyhz/animeus/SplashScreen.java
@@ -2,52 +2,24 @@ package id.rllyhz.animeus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 
-import id.rllyhz.animeus.api.ApiClient;
-import id.rllyhz.animeus.api.data_service.AnimeAPIService;
-import id.rllyhz.animeus.api.response_type.GetAnimeByIdResponseType;
-import id.rllyhz.animeus.api.response_type.GetCharacterByIdResponseType;
-import id.rllyhz.animeus.api.response_type.GetMangaByIdResponseType;
-import id.rllyhz.animeus.api.response_type.GetPersonByIdResponseType;
-import id.rllyhz.animeus.api.response_type.GetTopCharactersResponseType;
-import id.rllyhz.animeus.helper.CustomToast;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import id.rllyhz.animeus.activity.MainActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splashscreen);
 
-        getSupportActionBar().setTitle("Rully");
+        getSupportActionBar().hide();
 
-        AnimeAPIService animeAPIService = ApiClient.getAnimeApiServiceInstance().create(AnimeAPIService.class);
-        Call<GetTopCharactersResponseType> call = animeAPIService.getTopCharacters();
-
-        call.enqueue(new Callback<GetTopCharactersResponseType>() {
-            @Override
-            public void onResponse(Call<GetTopCharactersResponseType> call, Response<GetTopCharactersResponseType> response) {
-                if (response.isSuccessful()) {
-                    processData(response.body());
-                } else {
-                    CustomToast.shortToast(SplashScreen.this, response.message());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<GetTopCharactersResponseType> call, Throwable t) {
-                CustomToast.shortToast(SplashScreen.this, t.getMessage());
-            }
-        });
-    }
-
-    private void processData(GetTopCharactersResponseType topCharacters) {
-        String topFirstCharacterTitle = topCharacters.getCharacterList().get(0).getTitle();
-        CustomToast.longToast(getApplicationContext(), topFirstCharacterTitle);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        }, 3000);
     }
 }
