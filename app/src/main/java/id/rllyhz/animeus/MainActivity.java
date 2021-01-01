679f29efe966966c08c81417643eee7da7d2a5b1
rllyhz <rullyihza00@gmail.com>
2021-01-01 17:17:47 +0700

    Updating GetAnimeByIdResponse.java file

--- app/src/main/java/id/rllyhz/animeus/MainActivity.java
+++ app/src/main/java/id/rllyhz/animeus/MainActivity.java
@@ -3,6 +3,14 @@ package id.rllyhz.animeus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import id.rllyhz.animeus.api.ApiClient;
+import id.rllyhz.animeus.api.data_service.AnimeAPIService;
+import id.rllyhz.animeus.api.response_type.GetAnimeByIdResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AnimeAPIService service = ApiClient.getApiClientInstance().create(AnimeAPIService.class);
+        Call<GetAnimeByIdResponse> call = service.getAnimeById();
+        call.enqueue(new Callback<GetAnimeByIdResponse>() {
+            @Override
+            public void onResponse(Call<GetAnimeByIdResponse> call, Response<GetAnimeByIdResponse> response) {
+                getData(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<GetAnimeByIdResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void getData(GetAnimeByIdResponse body) {
+        if (body != null) {
+//            Log.d("TEST", body.)
+        }
     }
 }
--- app/src/main/java/id/rllyhz/animeus/api/data_service/AnimeAPIService.java
+++ app/src/main/java/id/rllyhz/animeus/api/data_service/AnimeAPIService.java
@@ -3,10 +3,9 @@ package id.rllyhz.animeus.api.data_service;
 import id.rllyhz.animeus.api.response_type.GetAnimeByIdResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface AnimeAPIService {
 
-    @GET("/anime/{id}/")
-    Call<GetAnimeByIdResponse> getAnimeById(@Query("id") int id);
+    @GET("/anime/343")
+    Call<GetAnimeByIdResponse> getAnimeById();
 }
