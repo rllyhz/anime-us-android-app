d97411632298b67c4c4d1ce512bdb7e382e88f04
rllyhz <rullyihza00@gmail.com>
2021-01-01 16:51:09 +0700

    Adding Data Service file for defining endpoints in retrofit

--- app/src/main/java/id/rllyhz/animeus/api/ApiClient.java
+++ app/src/main/java/id/rllyhz/animeus/api/ApiClient.java
@@ -1,4 +1,20 @@
 package id.rllyhz.animeus.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+    private static Retrofit retrofit;
+    public static final String BASEURL = "https://api.jikan.moe/v3/";
+
+    public static Retrofit getApiClientInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASEURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
 }
---
+++ app/src/main/java/id/rllyhz/animeus/api/data_service/AnimeAPIService.java
@@ -0,0 +1,4 @@
+package id.rllyhz.animeus.api.data_service;
+
+public interface AnimeAPIService {
+}
