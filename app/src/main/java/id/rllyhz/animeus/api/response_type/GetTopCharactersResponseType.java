8c555ace5e1f72e099e1dc6eeea9e3a0cadd3aa5
rllyhz <rullyihza00@gmail.com>
2021-01-01 22:37:13 +0700

    Rename GetTopCharactersResponse.java file to GetTopCharactersResponseType.java

--- app/src/main/java/id/rllyhz/animeus/api/response_type/GetTopCharactersResponseType.java
+++ app/src/main/java/id/rllyhz/animeus/api/response_type/GetTopCharactersResponseType.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class GetTopCharactersResponse {
+public class GetTopCharactersResponseType {
     @SerializedName("request_hash")
     @Expose
     private String requestHash;
