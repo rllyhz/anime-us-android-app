2acb2ca55d1b1317a747927e5eadcd23552da0ed
rllyhz <rullyihza00@gmail.com>
2021-01-01 17:04:25 +0700

    Adding Response Type file for retrieving anime by id

--- app/src/main/java/id/rllyhz/animeus/api/data_service/AnimeAPIService.java
+++ app/src/main/java/id/rllyhz/animeus/api/data_service/AnimeAPIService.java
@@ -1,10 +1,12 @@
 package id.rllyhz.animeus.api.data_service;
 
+import id.rllyhz.animeus.api.response_type.GetAnimeByIdResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface AnimeAPIService {
 
     @GET("/anime/{id}/")
-    Call
+    Call<GetAnimeByIdResponse> getAnimeById(@Query("id") int id);
 }
--- app/src/main/java/id/rllyhz/animeus/api/response_type/GetAnimeByIdResponse.java
+++ app/src/main/java/id/rllyhz/animeus/api/response_type/GetAnimeByIdResponse.java
@@ -1,4 +1,83 @@
 package id.rllyhz.animeus.api.response_type;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+import okhttp3.Response;
+
 public class GetAnimeByIdResponse {
+
+    @SerializedName("mal_id")
+    private int id;
+
+    @SerializedName("image_url")
+    private String imageUrl;
+
+    @SerializedName("trailer_url")
+    private String trailerUrl;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("title_english")
+    private String titleEnglish;
+
+
+    @SerializedName("title_japanese")
+    private String titleJapanese;
+
+    @SerializedName("type")
+    private String type;
+
+    @SerializedName("source")
+    private String source;
+
+    @SerializedName("episodes")
+    private int episodes;
+
+    @SerializedName("status")
+    private String status;
+
+    @SerializedName("airing")
+    private boolean isAiring;
+
+    @SerializedName("aired")
+    private Response airedOn;
+
+    @SerializedName("duration")
+    private String duration;
+
+    @SerializedName("rating")
+    private String rating;
+
+    @SerializedName("score")
+    private double score;
+
+    @SerializedName("rank")
+    private int rank;
+
+    @SerializedName("synopsis")
+    private double synopsis;
+
+    @SerializedName("premired")
+    private String premiredIn;
+
+    @SerializedName("producers")
+    private Response producers;
+
+    @SerializedName("licensors")
+    private Response licensors;
+
+    @SerializedName("studios")
+    private Response studios;
+
+    @SerializedName("genres")
+    private Response genres;
+
+    @SerializedName("opening_themes")
+    private ArrayList<String> openingThemes;
+
+    @SerializedName("ending_themes")
+    private ArrayList<String> endingThemes;
 }
