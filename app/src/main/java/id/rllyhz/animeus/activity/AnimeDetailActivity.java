c435a9580323269fb762b093781c64696167abb1
rllyhz <rullyihza00@gmail.com>
2021-01-26 15:51:09 +0700

    Adding AnimeDetailActivity.java and activity_anime_detail.xml files

---
+++ app/src/main/java/id/rllyhz/animeus/activity/AnimeDetailActivity.java
@@ -0,0 +1,16 @@
+package id.rllyhz.animeus.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.rllyhz.animeus.R;
+
+public class AnimeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anime_detail);
+    }
+}
