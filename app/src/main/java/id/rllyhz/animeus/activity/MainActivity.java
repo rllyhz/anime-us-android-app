e20c2507dc8314cfdb62f0792417cd93f05d42f6
rllyhz <rullyihza00@gmail.com>
2021-01-26 13:09:14 +0700

    Adding drawer_menu_header_layout.xml and toolbar tag in activity_main.xml

--- app/src/main/java/id/rllyhz/animeus/activity/MainActivity.java
+++ app/src/main/java/id/rllyhz/animeus/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import id.rllyhz.animeus.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setToolbar();
+
         // SEARCH endpoint hasn't been implemented yet!
 
         // type: (anime, manga, character, person)
         // https://api.jikan.moe/v3/search/type?q=Fate/Zero&page=1
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar_layout);
+        this.setSupportActionBar(toolbar);
     }
 }
