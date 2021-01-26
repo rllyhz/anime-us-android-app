53dd9a433296bfc81c395d2e6c787a06a7f9930c
rllyhz <rullyihza00@gmail.com>
2021-01-26 13:20:30 +0700

    Adding drawer_menu_header_layout.xml and toolbar tag in activity_main.xml

--- app/src/main/java/id/rllyhz/animeus/activity/MainActivity.java
+++ app/src/main/java/id/rllyhz/animeus/activity/MainActivity.java
@@ -3,18 +3,25 @@ package id.rllyhz.animeus.activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import id.rllyhz.animeus.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setToolbar();
+        initDrawerMenu();
 
         // SEARCH endpoint hasn't been implemented yet!
 
@@ -22,8 +29,25 @@ public class MainActivity extends AppCompatActivity {
         // https://api.jikan.moe/v3/search/type?q=Fate/Zero&page=1
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void initDrawerMenu() {
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
     private void setToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar_layout);
+        toolbar = findViewById(R.id.toolbar_layout);
         this.setSupportActionBar(toolbar);
     }
 }
